d8b11446a56030a814f0d0983c78087da9216554
Olfdev <kcn94@free.fr>
2023-11-27 16:53:45 +0100

    updated files

--- src/pages/error.js
+++ src/pages/error.js
@@ -1,7 +1,11 @@
-export default function error() {
-    const error = "Error Page"
+import {Link} from 'react-router-dom'
 
+export default function error() {
     return (
-        <p>{error}</p>
+        <>
+            <h1 className="error">404</h1>
+            <h3 className="error-oops">Oups ! La page que vous demandez n'existe pas.</h3>
+            <p className="error-home"><Link to='/'>Retourner à la page d'accueil</Link></p>
+        </>
     )
 }
