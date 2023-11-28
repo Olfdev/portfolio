5c66128ed1edb9f93b52dfb72a6fbe9b6e6a11e9
Olfdev <kcn94@free.fr>
2023-11-28 12:28:28 +0100

    updated responsive code

--- src/pages/error.js
+++ src/pages/error.js
@@ -3,7 +3,7 @@ import {Link} from 'react-router-dom'
 export default function error() {
     return (
         <>
-            <h1 className="error">404</h1>
+            <h1 className="error-page">404</h1>
             <h3 className="error-oops">Oups ! La page que vous demandez n'existe pas.</h3>
             <p className="error-home"><Link to='/'>Retourner à la page d'accueil</Link></p>
         </>
