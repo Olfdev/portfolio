9487ab40b3f688c71de1bc870c1602aade6cd821
Olfdev <kcn94@free.fr>
2024-03-20 00:54:59 +0100

    added icons

--- src/components/footer.js
+++ src/components/footer.js
@@ -3,7 +3,7 @@ export default function footer() {
     return (
         <footer>
             <p>Fait avec beaucoup de ❤️, pas mal de 🍔🍟 et peu de 💤</p>
-            <p>Dernière mise à jour : Novembre 2023</p>
+            <p>Dernière mise à jour : Mars 2024</p>
         </footer>
     )
 }
